4bfae0c388228aefdcbadfdc9354af36a1fc832c
marcialwushu <marcialwushu@gmail.com>
2018-02-22 11:53:48 -0300

    Dispĺaynig Shopping Items

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.services';
+import { Observable } from 'rxjs/Observable';
+import { Item } from "./../../models/item/item.model";
 
 @IonicPage()
 @Component({
@@ -8,8 +11,23 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>;
+
+  constructor(public navCtrl: NavController,
+    private shopping: ShoppingListService) {
+    this.shoppingList$ = this.shopping
+      .getshoppingList() //DB LIST
+      .snapshotChanges() // Key and Value 
+      .map(
+        changes => {
+          return changes.map(c => ({
+            key: c.payload.key,
+            ...c.payload.val(),
+          }));
+        }
+      );
 
   }
 
+  
 }
